a6021ccad0ad910b0dbff2c3a115b6c4f89fb5fe
Harip-69 <hariprasath.2301102@srec.ac.in>
2026-04-15 10:29:28 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Attendance Tracker 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
